601b9185a1a0b48cd5981a7cbea078a5d46ca6dc
Yevgeny Kaushnian <kefbox@gmail.com>
2019-08-18 22:39:22 -0400

    Serve client html from /build folder

--- app.js
+++ app.js
@@ -1,10 +1,16 @@
 import express from 'express';
+import { dirname } from 'path';
+import { fileURLToPath } from 'url';
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
 
 const app = express();
 const port = 5000;
 
+app.use(express.static(`${__dirname}/client/build/`));
+
 app.get('/', (req, res) => {
-  res.send('Hello World!');
+  res.sendFile(`${__dirname}/client/build/index.html`);
 });
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
