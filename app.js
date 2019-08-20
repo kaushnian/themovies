b07af8c6933923fd9d1cba104309e89b58370e3a
Yevgeny Kaushnian <kefbox@gmail.com>
2019-08-19 20:59:40 -0400

    Prepare to deploy

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ import { fileURLToPath } from 'url';
 const __dirname = dirname(fileURLToPath(import.meta.url));
 
 const app = express();
-const port = 5000;
+const port = process.env.PORT || 5000;
 
 app.use(express.static(`${__dirname}/client/build/`));
 
